cb2f4895cf2d7fbcf8dc38dd3aa990a2fe0b4678
Denis <denis.telpesov@isa.utm.md>
2023-10-18 08:39:36 -0700

    Add Domain

---
+++ Endava.TechCourse.BankApp.Domain/Common/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Endava.TechCourse.BankApp.Domain.Common
+{
+    internal class BaseEntity
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public DateTime TimeStap { get; } = DateTime.Now;
+    }
+}
