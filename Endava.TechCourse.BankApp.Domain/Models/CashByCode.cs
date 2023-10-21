3dbee5cc0015da2ad5ac4a5fd3c2be7dbf63ad10
Denis <denis.telpesov@isa.utm.md>
2023-10-21 04:09:09 -0700

    add "Transaction, CashByCode, Report"

---
+++ Endava.TechCourse.BankApp.Domain/Models/CashByCode.cs
@@ -0,0 +1,13 @@
+﻿using Endava.TechCourse.BankApp.Domain.Common;
+
+namespace Endava.TechCourse.BankApp.Domain.Models
+{
+    internal class CashByCode : BaseEntity
+    {
+        public Guid SourceWalletId { get; set; }
+        public string SourceUserId { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+        public string GeneratedCode { get; set; }
+    }
+}
---
+++ Endava.TechCourse.BankApp.Domain/Models/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using Endava.TechCourse.BankApp.Domain.Common;
+
+namespace Endava.TechCourse.BankApp.Domain.Models
+{
+    internal class Transaction : BaseEntity
+    {
+        public Guid SourceWalletId { get; set; }
+        public string SourceUserId { get; set; }
+        public string SourceUserName { get; set; }
+        public Guid DestinationWalletId { get; set; }
+        public string DestinationUserId { get; set; }
+        public string DestinationUserName { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+    }
+}
