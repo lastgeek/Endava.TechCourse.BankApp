4fa06c74599b168c191e2dcf4d1965fb0f570829
Denis <denis.telpesov@isa.utm.md>
2023-10-28 18:28:45 -0700

    Create DB

---
+++ Server/Controllers/WalletController.cs
@@ -0,0 +1,46 @@
+﻿using Endava.TechCourse.BankApp.Domain.Models;
+using Endava.TechCourse.BankApp.Infrastracture.Persistance;
+using Endava.TechCourse.BankApp.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Endava.TechCourse.BankApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalletController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public WalletController(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        [HttpPost]
+        public IActionResult CreateWallet([FromBody] CreateWalletDTO createWalletDTO)
+        {
+            var wallet = new Wallet
+            {
+                Type = createWalletDTO.Type,
+                Amount = createWalletDTO.Amount,
+                Currency = new Currency
+                {
+                    Name = "EURO",
+                    CurrencyCode = "EURO",
+                    CurrencyRate = 20,
+                }
+            };
+            _context.Wallets.Add(wallet);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("getwallets")]
+        public async Task<List<Wallet>> GetWallets()
+        {
+            return await _context.Wallets.ToListAsync();
+        }
+    }
+}
