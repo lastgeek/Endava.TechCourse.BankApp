fcaa012dbd124321dd7230085db1615cb2a50ed5
Denis <denis.telpesov@isa.utm.md>
2023-10-30 13:23:28 -0700

    Refactoring

---
+++ Shared/WalletDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Endava.TechCourse.BankApp.Shared
+{
+    public class WalletDTO
+    {
+        public string Id { get; set; }
+        public string Currency { get; set; }
+        public string Type { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
