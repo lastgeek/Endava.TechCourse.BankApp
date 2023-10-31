bf08573a6dd717be2a12ef47457926004c50c0c4
Denis <denis.telpesov@isa.utm.md>
2023-10-31 14:11:36 -0700

    Add unit test project

---
+++ Endava.TechCourse.BankApp.Test/ControllersTests/WalletControllerTests.cs
@@ -0,0 +1,69 @@
+﻿using Endava.TechCourse.BankApp.Domain.Models;
+using Endava.TechCourse.BankApp.Infrastracture.Persistance;
+using Endava.TechCourse.BankApp.Server.Controllers;
+using Endava.TechCourse.BankApp.Shared;
+using Endava.TechCourse.BankApp.Test.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Endava.TechCourse.BankApp.Test.ControllersTests
+{
+    public class WalletControllerTests
+    {
+        [Test, ApplicationData]
+        public void CanCreateInstance(GuardClauseAssertion assertion)
+            => assertion.Verify(typeof(WalletController).GetConstructors());
+
+        [Test, ApplicationData]
+        public async Task ShouldGetWallets(
+            [Frozen] ApplicationDbContext context,
+            [Greedy] WalletController controller,
+            Wallet firstWallet,
+            Wallet secondWallet)
+        {
+            //Arrange
+            context.Wallets.AddRange(firstWallet, secondWallet);
+            context.SaveChanges();
+            context.ChangeTracker.Clear();
+
+            //Act
+            var result = await controller.GetWallets();
+
+            //Assert
+            result.Count.Should().Be(2);
+        }
+
+        [Test, ApplicationData]
+        public void ShouldReturnOkForValidId(
+            [Frozen] ApplicationDbContext context,
+            [Greedy] WalletController controller,
+            Wallet wallet)
+        {
+            // Arrange
+            context.Wallets.Add(wallet);
+            context.SaveChanges();
+            context.ChangeTracker.Clear();
+
+            // Act
+            var result = controller.GetWalletDetails(wallet.Id);
+
+            // Assert
+            Assert.IsInstanceOf<ActionResult<WalletDTO>>(result);
+            var actionResult = result as ActionResult<WalletDTO>;
+            Assert.IsInstanceOf<OkObjectResult>(actionResult.Result);
+        }
+
+        [Test, ApplicationData]
+        public void ShouldReturnNotFoundForInvalidId(
+            [Frozen] ApplicationDbContext context,
+            [Greedy] WalletController controller)
+        {
+            // Act
+            var result = controller.GetWalletDetails(Guid.NewGuid());
+
+            // Assert
+            Assert.IsInstanceOf<ActionResult<WalletDTO>>(result);
+            var actionResult = result as ActionResult<WalletDTO>;
+            Assert.IsInstanceOf<NotFoundResult>(actionResult.Result);
+        }
+    }
+}
---
+++ Endava.TechCourse.BankApp.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Endava.TechCourse.BankApp.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
